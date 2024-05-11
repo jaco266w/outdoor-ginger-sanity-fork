03ad6b31b054a7e0042ca570bed8f1fa514623e1
jaco266w <jaco266w@stud.kea.dk>
2024-05-11 06:50:16 +0200

    sponsor valication + url

--- schemaTypes/sponsor.js
+++ schemaTypes/sponsor.js
@@ -8,9 +8,27 @@ export const sponsor = defineType({
             type: "string",
             name: "Sponsor_Name",
         }),
-      defineField({
-        type: "image",
-        name: "Sponsor_Logo",
-      }),
+        defineField({
+            type: "string",
+            name: "Sponsor_Website",
+            description: "Please include the full URL (e.g. https://www.example.com)",
+            validation: (Rule) => Rule.uri({allowRelative: false})
+        }),
+        defineField({
+          type: "image",
+          name: "Sponsor_Logo",
+          description: "Please only upload white logos with transparent backgrounds.(PNG/WEBP file only)",
+          validation: (Rule) => Rule.custom((fieldValue, context) => {
+            if (fieldValue && fieldValue.asset) {
+              const assetRefParts = fieldValue.asset._ref.split('-');
+              const extensionPart = assetRefParts[assetRefParts.length - 1];
+              const extension = extensionPart.split('.')[0];
+              if (extension !== 'png' && extension !== 'webp') {
+                return 'Only PNG and WEBP files are allowed';
+              }
+            }
+            return true;
+          })
+        }),
     ],
     });
